f6405c5154e56d29f6088f52e4660f177dec86b2
Theodore ATEBA <tf.ateba@gmail.com>
2016-07-09 22:16:40 +0200

    Add file informations and comment.

--- main.c
+++ main.c
@@ -1,31 +1,35 @@
-/*
-    ChibiOS - Copyright (C) 2006..2015 Giovanni Di Sirio
+/**
+ * @file  main.c
+ *
+ * @brief Main file for the application.
+ *
+ * @author  Theodore Ateba
+ *
+ * @version 1.0
+ *
+ * @date    30 December 2015
+ *
+ * @update  09 Jully 2016
+ *
+ */
 
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-
-        http://www.apache.org/licenses/LICENSE-2.0
-
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
-*/
-
-/*
-* Includes libraries files
-*/
+/*===========================================================================*/
+/* Includes libraries files                                                  */
+/*===========================================================================*/
 #include "ch.h"
 #include "hal.h"
 #include "chprintf.h"
 
-/*
- * Variables:
- */
-BaseSequentialStream* pc = (BaseSequentialStream*) &SD2;
-uint16_t counter = 0;
+/*===========================================================================*/
+/* Variables                                                                 */
+/*===========================================================================*/
+
+BaseSequentialStream* pc = (BaseSequentialStream*) &SD2; /**< For print      */
+uint16_t counter = 0;                                     /**< Simple counter*/
+
+/*===========================================================================*/
+/* Function and thread function                                              */
+/*===========================================================================*/
 
 /*
  * LED blinker thread, times are in milliseconds.
