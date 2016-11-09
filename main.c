3cd6d193f421c1e7adecc079a97b8ccb09f5cefa
Theodore Ateba <tf.ateba@gmail.com>
2016-11-09 07:07:55 +0100

    Code style fixed.

--- main.c
+++ main.c
@@ -1,49 +1,48 @@
 /**
- * @file  main.c
+ * @file    main.c
  *
- * @brief Main file for the application.
+ * @brief   Main file for the application.
  *
- * @author  Theodore Ateba
+ * @author  Theodore Ateba, tf.ateba@gmail.com
  *
  * @version 1.0
  *
  * @date    30 December 2015
  *
- * @update  09 Jully 2016
+ * @update  09 November 2016
  *
  */
 
 /*===========================================================================*/
-/* Includes libraries files                                                  */
+/* Includes libraries files.                                                 */
 /*===========================================================================*/
 #include "ch.h"
 #include "hal.h"
 #include "chprintf.h"
 
 /*===========================================================================*/
-/* Variables                                                                 */
+/* Local Variables.                                                          */
 /*===========================================================================*/
-
-BaseSequentialStream* pc = (BaseSequentialStream*) &SD2; /**< For print      */
+BaseSequentialStream* pc = (BaseSequentialStream*) &SD2;  /**< For print     */
 uint16_t counter = 0;                                     /**< Simple counter*/
 
 /*===========================================================================*/
-/* Function and thread function                                              */
+/* Function and thread function.                                             */
 /*===========================================================================*/
 
 /*
- * LED blinker thread, times are in milliseconds.
+ * @biref LED blinker thread.
  */
 static THD_WORKING_AREA(waThread1, 128);
-static THD_FUNCTION(Thread1, arg){
+static THD_FUNCTION(Thread1, arg) {
   (void)arg;
-  
+
   chRegSetThreadName("blinker");
   systime_t time = chVTGetSystemTimeX();
-  while (true){
+  while (true) {
     time += MS2ST(1000);
     palTogglePad(GPIOA, GPIOA_LED_GREEN);
-    chprintf(pc, "\n\rfox: Hello - %d", counter++);
+    chprintf(pc, "\n\rHello - %d", counter++);
     chThdSleepUntil(time);
   }
 }
@@ -51,7 +50,7 @@ static THD_FUNCTION(Thread1, arg){
 /*
  * Application entry point.
  */
-int main(void){
+int main(void) {
   /*
    * System initializations.
    * - HAL initialization, this also initializes the configured device drivers
@@ -61,24 +60,27 @@ int main(void){
    */
   halInit();
   chSysInit();
-  
+
   /*
    * Init Serial Driver 2 using the default configuration
    */
   sdStart(&SD2, NULL);
-  chprintf(pc, "\n\r\n\rfox: Init serial port.");
-  
+  chprintf(pc, "\n\r\n\r Init serial port.");
+
   /*
    * Creates the blinker thread.
    */
-  chprintf(pc, "\n\rfox: Create thread to blink the onboard LED2, increment a\
+  chprintf(pc, "\n\r Create thread to blink the onboard LED2, increment a\
       counter and print it to the serial port 2 (USB).");
   chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, Thread1, NULL);
-  
+
   /*
    * Normal main() thread activity, in this demo it does nothing.
    */
-  while (true){
+  while (true) {
     chThdSleepMilliseconds(1000);
   }
+
+  return 0;
 }
+
